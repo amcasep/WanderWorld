ea967da97de1466d6a61b56104f62bb78f3ea5dd
Amparo Cabezuelo Sepulveda <amcasep@gmail.com>
2024-03-01 08:21:19 +0100

    fixed minor thigs: replaced video, picture in subscribe section

--- src/Components/Subscribe/Subscribe.jsx
+++ src/Components/Subscribe/Subscribe.jsx
@@ -5,7 +5,10 @@ const Subscribe = () => {
     return (
         <div className="Subscribe section container">
             <div className="secContainer grid">
-                <img src={img1} alt="Lady on phone" />
+                <div className="imgDiv">
+                    <img src={img1} alt="Lady on phone" />
+                </div>
+
                 <div className="textDiv">
                     <h4>Best Way to Start Your Journey!</h4>
                     <p>We offer personalized itineraries tailored to individual preferences and interests.</p>
@@ -15,5 +18,5 @@ const Subscribe = () => {
         </div>
     );
 }
- 
+
 export default Subscribe;
