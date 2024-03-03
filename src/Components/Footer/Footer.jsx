cb70eb1a52716babbab9e284afc32bd077f6e601
Amparo Cabezuelo Sepulveda <amcasep@gmail.com>
2024-03-03 19:36:11 +0100

    fixed footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -13,46 +13,47 @@ const Footer = () => {
                         <CgAirplane className="icon" />
                         <span>WanderWorld</span>
                     </div>
-                    <div className="socials flex">
-                        <ImFacebook className="icon" />
-                        <BsTwitterX className="icon" />
-                        <AiFillInstagram className="icon" />
-                    </div>
-                    <div className="footerLinks">
-                        <span className="linkTitle">Information</span>
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">Explore</a>
-                        </li>
-                        <li>
-                            <a href="#">Travel</a>
-                        </li>
-                        <li>
-                            <a href="#">Blog</a>
-                        </li>
-                    </div>
-                    <div className="footerLinks">
-                        <span className="linkTitle">Helpful Links</span>
-                        <li>
-                            <a href="#">Destination</a>
-                        </li>
-                        <li>
-                            <a href="#">Support</a>
-                        </li>
-                        <li>
-                            <a href="#">Travel & Conditions</a>
-                        </li>
-                        <li>
-                            <a href="#">Privacy</a>
-                        </li>
-                    </div>
-                    <div className="footerLinks">
-                        <span className="linkTitle">Contact Details</span>
-                        <span className="phone">+657 254 297</span>
-                        <span className="email">email@email.com</span>
-                    </div>
+                </div>
+                <div className="socials flex">
+                    <ImFacebook className="icon" />
+                    <BsTwitterX className="icon" />
+                    <AiFillInstagram className="icon" />
+                </div>
+                <div className="footerLinks">
+                    <span className="linkTitle">Information</span>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Explore</a>
+                    </li>
+                    <li>
+                        <a href="#">Travel</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                </div>
+                <div className="footerLinks">
+                    <span className="linkTitle">Helpful Links</span>
+                    <li>
+                        <a href="#">Destination</a>
+                    </li>
+                    <li>
+                        <a href="#">Support</a>
+                    </li>
+                    <li>
+                        <a href="#">Travel & Conditions</a>
+                    </li>
+                    <li>
+                        <a href="#">Privacy</a>
+                    </li>
+                </div>
+                <div className="footerLinks">
+                    <span className="linkTitle">Contact Details</span>
+                    <span className="phone">+657 254 297</span>
+                    <span className="email">email@email.com</span>
+
                 </div>
             </div>
         </div>
